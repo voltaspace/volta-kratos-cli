ebe22da068b72a42a72368a117b268d764e1d1e2
volta <806269155@qq.com>
2022-12-11 23:06:11 +0800

    remove kratos/v2

--- kratos/internal/upgrade/upgrade.go
+++ kratos/internal/upgrade/upgrade.go
@@ -19,7 +19,6 @@ var CmdUpgrade = &cobra.Command{
 // Run upgrade the kratos tools.
 func Run(cmd *cobra.Command, args []string) {
 	err := base.GoInstall(
-		"github.com/go-kratos/kratos/cmd/kratos/v2",
 		"github.com/go-kratos/kratos/cmd/protoc-gen-go-http/v2",
 		"github.com/go-kratos/kratos/cmd/protoc-gen-go-errors/v2",
 		"google.golang.org/protobuf/cmd/protoc-gen-go",

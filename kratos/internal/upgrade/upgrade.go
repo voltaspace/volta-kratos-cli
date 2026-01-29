08d56ab32c3c43cc7efae630bfe5a44dd71ba92e
volta <806269155@qq.com>
2026-01-29 10:53:29 +0800

    Update upgrade.go

--- kratos/internal/upgrade/upgrade.go
+++ kratos/internal/upgrade/upgrade.go
@@ -19,12 +19,12 @@ var CmdUpgrade = &cobra.Command{
 // Run upgrade the kratos tools.
 func Run(cmd *cobra.Command, args []string) {
 	err := base.GoInstall(
-		"github.com/go-kratos/kratos/cmd/protoc-gen-go-http/v2",
-		"github.com/go-kratos/kratos/cmd/protoc-gen-go-errors/v2",
-		"google.golang.org/protobuf/cmd/protoc-gen-go",
-		"github.com/favadi/protoc-go-inject-tag",
-		"google.golang.org/grpc/cmd/protoc-gen-go-grpc",
-		"github.com/envoyproxy/protoc-gen-validate",
+		"github.com/go-kratos/kratos/cmd/protoc-gen-go-http/v2@v2.1.3",
+		"github.com/go-kratos/kratos/cmd/protoc-gen-go-errors/v2@v2.1.3",
+		"google.golang.org/protobuf/cmd/protoc-gen-go@v1.33.0",
+		"github.com/favadi/protoc-go-inject-tag@v1.4.0",
+		"google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.42.0",
+		"github.com/envoyproxy/protoc-gen-validate@v0.1.0",
 	)
 	if err != nil {
 		fmt.Println(err)
